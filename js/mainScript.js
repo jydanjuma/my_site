6625c03e6594c6e3cb7827620f852a6de1af4568
Jubril Yusuf Danjuma <yuslovekate@gmail.com>
2023-12-24 00:18:16 +0100

    initial commit

---
+++ js/mainScript.js
@@ -0,0 +1,26 @@
+const sr = ScrollReveal ({
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  reset: true
+})
+
+
+sr.reveal('.second .container img',{delay: 800, origin: 'left'})
+//sr.reveal('.third .container ',{delay: 800, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg1 img',{delay: 600, origin: 'top'})
+sr.reveal('.fourth .projectImg .mainImg2 img',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg3 img',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg4 img',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg5',{delay: 400, origin: 'bottom'})
+sr.reveal('.fourth  .projectImg .mainImg6',{delay: 200, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg7',{delay: 800, origin: 'left'})
+sr.reveal('.fourth  .projectImg .mainImg8',{delay: 800, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg9',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg10',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg11',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg12',{delay: 600, origin: 'top'})
+sr.reveal('.fourth .projectImg .mainImg13',{delay: 400, origin: 'bottom'})
+sr.reveal('.fourth  .projectImg .mainImg14',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg15',{delay: 600, origin: 'top'})
+sr.reveal('.fourth  .projectImg .mainImg16',{delay: 400, origin: 'bottom'})
